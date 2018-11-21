939acdae5ce8ff603cb432021996fafb8e25a3e6
Alex Tesevich <alex.tesevich@gmail.com>
2018-11-21 13:43:13 +0300

    Добавил КОРС + массив картинок для продукта

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,26 +2,52 @@ Product.create([
   {
     title: 'Product 1',
     price: 10000,
-    image_urls: ['https://www.royalcanin.com/vet/~/media/Royal-Canin/Product-Categories/cat-kitten-landing-hero.ashx'],
+    image_urls: [
+      'https://www.royalcanin.com/vet/~/media/Royal-Canin/Product-Categories/cat-kitten-landing-hero.ashx',
+      'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg',
+      'https://images.pexels.com/photos/156934/pexels-photo-156934.jpeg'
+    ],
    },
    {
      title: 'Product 2',
      price: 15000,
-     image_urls: ['https://chemvet.dk/sites/default/files/cat.jpg'],
+     image_urls: [
+       'https://chemvet.dk/sites/default/files/cat.jpg',
+       'https://images.pexels.com/photos/20787/pexels-photo.jpg',
+       'https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg',
+       'https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg'
+     ],
    },
    {
      title: 'Product 3',
      price: 17000,
-     image_urls: ['https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx'],
+     image_urls: [
+       'https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx',
+       'https://images.pexels.com/photos/923360/pexels-photo-923360.jpeg',
+       'https://images.pexels.com/photos/126407/pexels-photo-126407.jpeg',
+       'https://images.pexels.com/photos/33537/cat-animal-cat-portrait-mackerel.jpg'
+     ],
    },
    {
      title: 'Product 4',
      price: 21000,
-     image_urls: ['https://cdn.arstechnica.net/wp-content/uploads/2017/06/Fotolia_58941902_Subscription_Monthly_M.jpg'],
+     image_urls: [
+       'https://cdn.arstechnica.net/wp-content/uploads/2017/06/Fotolia_58941902_Subscription_Monthly_M.jpg',
+       'https://images.pexels.com/photos/385960/pexels-photo-385960.jpeg',
+       'https://images.pexels.com/photos/115011/cat-face-close-view-115011.jpeg',
+       'https://images.pexels.com/photos/1084425/pexels-photo-1084425.jpeg',
+       'https://images.pexels.com/photos/1331821/cat-eyes-angry-suspicious-1331821.jpeg'
+     ],
    },
    {
      title: 'Product 5',
      price: 28000,
-     image_urls: ['https://cdn1-www.cattime.com/assets/uploads/2011/12/file_2738_american-bobtail-460x290.jpg'],
+     image_urls: [
+       'https://cdn1-www.cattime.com/assets/uploads/2011/12/file_2738_american-bobtail-460x290.jpg',
+       'https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg',
+       'https://images.pexels.com/photos/137049/pexels-photo-137049.jpeg',
+       'https://images.pexels.com/photos/302280/pexels-photo-302280.jpeg',
+       'https://images.pexels.com/photos/69932/tabby-cat-close-up-portrait-69932.jpeg'
+     ],
    }
 ])
